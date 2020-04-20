36750a427c1d411798ab79e8f352bcad17998574
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-20 16:48:18 -0500

    [Feature] upload task 0,1 and readme file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world2():
+    return 'HBNB'
+
+app.run(host='0.0.0.0', port=5000)
