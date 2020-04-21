8fe39c066e0d96d6d41939bc8d0641ce90b6aa9f
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-21 12:17:58 -0500

    [Fix] if name = main

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,4 +9,6 @@ def hello_world():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    """main"""
+    app.run(host='0.0.0.0', port=5000)
